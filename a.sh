1aa5a88b5562f7365aa60798eeba0065a0010529
anandpavithran <58514903+anandpavithran@users.noreply.github.com>
2024-06-07 12:20:08 +0530

    Update a.sh

--- a.sh
+++ a.sh
@@ -3,5 +3,6 @@ lab start convert-plan
 ssh root@servera cp /tmp/convert2rhel.repo /etc/yum.repos.d/
 ssh root@servera yum update -y 
 ssh root@servera reboot
-sleep 60
+sleep 30
+ssh root@servera yum install convert2rhel -y
 lab finish convert-plan
